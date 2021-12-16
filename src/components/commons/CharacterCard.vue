b5d97fcc9c071393a70fc84893ededf53db73d55
Giovanni <gio.a.98@gmail.com>
2021-12-16 12:33:21 +0100

    milestone 1

---
+++ src/components/commons/CharacterCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="character text-center">
+        <img class="w-100 mb-3" :src="info.image" :alt="info.name + ' avatar'">
+        <h4>{{info.name}}</h4>
+        <hr class="mx-auto">
+        <p>{{info.origin}}</p>
+        <p><strong>{{info.species}}</strong></p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CharacterCard",
+    props: {
+        info: Object
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.character {
+    img {
+        border-radius: 50%;
+        box-shadow: 0 0 .5rem lightgrey;
+    }
+    hr {
+        width: 60px;
+        opacity: 1;
+    }
+}
+</style>
---
+++ src/components/sections/CharactersList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-6 col-lg-3 mb-5" v-for="character in characters" :key="character.id">
+                <CharacterCard :info="character"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import CharacterCard from '../commons/CharacterCard.vue';
+export default {
+    name: "CharactersList",
+    components: {
+        CharacterCard
+    },
+    data() {
+        return {
+            characters: null
+        }
+    },
+    created() {
+        axios.get("https://api.sampleapis.com/rickandmorty/characters")
+        .then((response) => {
+            this.characters = response.data;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+h2 {
+    text-align: center;
+}
+</style>
